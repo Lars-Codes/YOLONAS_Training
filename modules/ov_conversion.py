12d8c274c675eb755c1a276d79cc0bc248ddb2c0
Lars <lara.palombi@gmail.com>
2024-06-10 11:49:37 -0400

    change name

--- modules/ov_conversion.py
+++ modules/ov_conversion.py
@@ -1,7 +1,8 @@
 import openvino as ov 
 import torch 
+import os
 
-model_path = 'model/path'
+model_path = './modules/average_model.pth'
 model = torch.load(model_path)
 
 ov_model = ov.convert_model(model)
